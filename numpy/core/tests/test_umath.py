numpy/numpy

TST: fix RuntimeWarnings due to invalid values in umath tests.

numpy/core/tests/test_umath.py
@@ -5,6 +5,14 @@ import numpy.core.umath as ncu
 import numpy as np
 
 
+class _FilterInvalids():
+    def setUp(self):
+        self.olderr = np.seterr(invalid='ignore')
+
+    def tearDown(self):
+        np.seterr(**olderr)
+
+
 class TestDivision(TestCase):
     def test_division_int(self):
         # int division should follow Python
@@ -152,7 +160,7 @@ class TestExp2(TestCase):
             assert_almost_equal(np.exp2(yf), xf)
 
 
-class TestLogAddExp2(object):
+class TestLogAddExp2(TestCase, _FilterInvalids):
     # Need test for intermediate precisions
     def test_logaddexp2_values(self) :
         x = [1, 2, 3, 4, 5]
@@ -219,7 +227,7 @@ class TestExp(TestCase):
             assert_almost_equal(np.exp(yf), xf)
 
 
-class TestLogAddExp(object):
+class TestLogAddExp(TestCase, _FilterInvalids):
     def test_logaddexp_values(self) :
         x = [1, 2, 3, 4, 5]
         y = [5, 4, 3, 2, 1]
@@ -432,7 +440,7 @@ class TestLdexp(TestCase):
             np.seterr(**err)
 
 
-class TestMaximum(TestCase):
+class TestMaximum(TestCase, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -479,7 +487,7 @@ class TestMaximum(TestCase):
         assert_equal(np.maximum(arg1, arg2), arg2)
 
 
-class TestMinimum(TestCase):
+class TestMinimum(TestCase, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -526,7 +534,7 @@ class TestMinimum(TestCase):
         assert_equal(np.minimum(arg1, arg2), arg1)
 
 
-class TestFmax(TestCase):
+class TestFmax(TestCase, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -568,7 +576,7 @@ class TestFmax(TestCase):
             assert_equal(np.fmax(arg1, arg2), out)
 
 
-class TestFmin(TestCase):
+class TestFmin(TestCase, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -1217,19 +1225,24 @@ def test_complex_nan_comparisons():
     fins = [complex(1, 0), complex(-1, 0), complex(0, 1), complex(0, -1),
             complex(1, 1), complex(-1, -1), complex(0, 0)]
 
-    for x in nans + fins:
-        x = np.array([x])
-        for y in nans + fins:
-            y = np.array([y])
+    olderr = np.seterr(invalid='ignore')
+    try:
+        for x in nans + fins:
+            x = np.array([x])
+            for y in nans + fins:
+                y = np.array([y])
 
-            if np.isfinite(x) and np.isfinite(y):
-                continue
+                if np.isfinite(x) and np.isfinite(y):
+                    continue
 
-            assert_equal(x < y, False, err_msg="%r < %r" % (x, y))
-            assert_equal(x > y, False, err_msg="%r > %r" % (x, y))
-            assert_equal(x <= y, False, err_msg="%r <= %r" % (x, y))
-            assert_equal(x >= y, False, err_msg="%r >= %r" % (x, y))
-            assert_equal(x == y, False, err_msg="%r == %r" % (x, y))
+                assert_equal(x < y, False, err_msg="%r < %r" % (x, y))
+                assert_equal(x > y, False, err_msg="%r > %r" % (x, y))
+                assert_equal(x <= y, False, err_msg="%r <= %r" % (x, y))
+                assert_equal(x >= y, False, err_msg="%r >= %r" % (x, y))
+                assert_equal(x == y, False, err_msg="%r == %r" % (x, y))
+    finally:
+        np.seterr(**olderr)
+
 
 if __name__ == "__main__":
     run_module_suite()

blacklegchef/numpy-mirror

numpy/core/tests/test_umath.py
@@ -5,6 +5,14 @@ import numpy.core.umath as ncu
 import numpy as np
 
 
+class _FilterInvalids():
+    def setUp(self):
+        self.olderr = np.seterr(invalid='ignore')
+
+    def tearDown(self):
+        np.seterr(**olderr)
+
+
 class TestDivision(TestCase):
     def test_division_int(self):
         # int division should follow Python
@@ -152,7 +160,7 @@ class TestExp2(TestCase):
             assert_almost_equal(np.exp2(yf), xf)
 
 
-class TestLogAddExp2(object):
+class TestLogAddExp2(object, _FilterInvalids):
     # Need test for intermediate precisions
     def test_logaddexp2_values(self) :
         x = [1, 2, 3, 4, 5]
@@ -219,7 +227,7 @@ class TestExp(TestCase):
             assert_almost_equal(np.exp(yf), xf)
 
 
-class TestLogAddExp(object):
+class TestLogAddExp(object, _FilterInvalids):
     def test_logaddexp_values(self) :
         x = [1, 2, 3, 4, 5]
         y = [5, 4, 3, 2, 1]
@@ -432,7 +440,7 @@ class TestLdexp(TestCase):
             np.seterr(**err)
 
 
-class TestMaximum(TestCase):
+class TestMaximum(object, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -479,7 +487,7 @@ class TestMaximum(TestCase):
         assert_equal(np.maximum(arg1, arg2), arg2)
 
 
-class TestMinimum(TestCase):
+class TestMinimum(object, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -526,7 +534,7 @@ class TestMinimum(TestCase):
         assert_equal(np.minimum(arg1, arg2), arg1)
 
 
-class TestFmax(TestCase):
+class TestFmax(object, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -568,7 +576,7 @@ class TestFmax(TestCase):
             assert_equal(np.fmax(arg1, arg2), out)
 
 
-class TestFmin(TestCase):
+class TestFmin(object, _FilterInvalids):
     def test_reduce(self):
         dflt = np.typecodes['AllFloat']
         dint = np.typecodes['AllInteger']
@@ -1217,19 +1225,24 @@ def test_complex_nan_comparisons():
     fins = [complex(1, 0), complex(-1, 0), complex(0, 1), complex(0, -1),
             complex(1, 1), complex(-1, -1), complex(0, 0)]
 
-    for x in nans + fins:
-        x = np.array([x])
-        for y in nans + fins:
-            y = np.array([y])
+    olderr = np.seterr(invalid='ignore')
+    try:
+        for x in nans + fins:
+            x = np.array([x])
+            for y in nans + fins:
+                y = np.array([y])
 
-            if np.isfinite(x) and np.isfinite(y):
-                continue
+                if np.isfinite(x) and np.isfinite(y):
+                    continue
 
-            assert_equal(x < y, False, err_msg="%r < %r" % (x, y))
-            assert_equal(x > y, False, err_msg="%r > %r" % (x, y))
-            assert_equal(x <= y, False, err_msg="%r <= %r" % (x, y))
-            assert_equal(x >= y, False, err_msg="%r >= %r" % (x, y))
-            assert_equal(x == y, False, err_msg="%r == %r" % (x, y))
+                assert_equal(x < y, False, err_msg="%r < %r" % (x, y))
+                assert_equal(x > y, False, err_msg="%r > %r" % (x, y))
+                assert_equal(x <= y, False, err_msg="%r <= %r" % (x, y))
+                assert_equal(x >= y, False, err_msg="%r >= %r" % (x, y))
+                assert_equal(x == y, False, err_msg="%r == %r" % (x, y))
+    finally:
+        np.seterr(**olderr)
+
 
 if __name__ == "__main__":
     run_module_suite()